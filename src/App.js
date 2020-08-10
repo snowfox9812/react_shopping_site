85afc6ca9322901b421750dd4b099b24a9ca78d6
trungnguyen <trung.duc.nguyen1298@gmail.com>
2020-08-10 09:30:33 +0700

    add main home components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/header.js'
 import Footer from './components/footer/footer.js'
+import Home from './components/bottom/home/home.js'
 
 function App() {
   return (
     <div> 
       <Header />
+      <Home />
       <Footer />
     </div>
   );
