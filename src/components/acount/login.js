7cbd8e1e89a94116869d99d91322a49a5b9fe8ef
Ly Trung Kien <lytrungkientq99@gmail.com>
2020-08-20 10:36:00 +0700

    update account

--- src/components/acount/login.js
+++ src/components/acount/login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import './account.css';
 
@@ -7,21 +7,22 @@ import { Form, Button } from 'react-bootstrap';
 import NikeLogo from '../../asset/img/icon/Nike_logo.jpg';
 import {
     Link,
-  } from "react-router-dom";
-import withFirebaseAuth from 'react-with-firebase-auth'
+} from "react-router-dom";
+
+import withFirebaseAuth from 'react-with-firebase-auth';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from '../../firebaseConfig';
 import 'firebase/firestore';
-import {signInWithEmailAndPassword} from 'firebase'
+import { signInWithEmailAndPassword } from 'firebase'
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
 const auth = firebaseApp.auth();
-const providers = { googleProvider: new firebase.auth.GoogleAuthProvider(),};
+const providers = { googleProvider: new firebase.auth.GoogleAuthProvider() };
 var db = firebase.firestore();
 
 function Login(props) {
-    const user = props.user;        
+    const user = props.user;
     const signOut = props.signOut;
     const signInWithGoogle = props.signInWithGoogle;
 
@@ -34,57 +35,68 @@ function Login(props) {
     //         console.log(`${doc.id} => ${doc.data()}`);
     //     });
     // });
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         // ...
-      });
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
     });
-    firebase.auth().signOut().then(function() {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+    });
+    firebase.auth().signOut().then(function () {
         // Sign-out successful.
-    }).catch(function(error) {
+    }).catch(function (error) {
         // An error happened.
     });
 
-  
 
-    const signInWithEmailAndPasswordHandler = (event,email, password) => {
+
+    const signInWithEmailAndPasswordHandler = (event, email, password) => {
         event.preventDefault();
         auth.signInWithEmailAndPassword(email.trim(), password.trim())
-        .catch(error => {
-        setError("Error signing in with password and email!");
-          console.error("Error signing in with password and email", error);
-        });
-        console.log(email,password)
-      };
-      
-      const onChangeHandler = (event) => {
-          const {name, value} = event.currentTarget;
-        
-          if(name === 'userEmail') {
-              setEmail(value);
-          }
-          else if(name === 'userPassword'){
+            .catch(error => {
+                setError("Error signing in with password and email!");
+                console.error("Error signing in with password and email", error);
+            });
+        console.log(email, password)
+    };
+
+    const onChangeHandler = (event) => {
+        const { name, value } = event.currentTarget;
+
+        if (name === 'userEmail') {
+            setEmail(value);
+        }
+        else if (name === 'userPassword') {
             setPassword(value);
-          }
-      };
+        }
+    };
+
+    const validateEmail = (event) => {
+        
+    }
 
     return (
         <div className="Create">
             <Link to="/"><img alt="" src={NikeLogo} width={50} /></Link>
             <h2>YOUR ACCOUNT FOR EVERYTHING NIKE</h2>
-            <Form onSubmit = {(event) => {signInWithEmailAndPasswordHandler(event, email.trim(), password.trim())}}>
+            <Form onSubmit={(event) => { signInWithEmailAndPasswordHandler(event, email.trim(), password.trim()) }}>
                 <Form.Group>
-                    <Form.Control type="email" name="email" placeholder="Enter email" id="userEmail" defaultValue={email} onChange = {(event) => onChangeHandler(event)} />
+                    <Form.Control
+                        type="email"
+                        name="email"
+                        placeholder="Enter email"
+                        id="userEmail"
+                        defaultValue={email}
+                        onChange={(event) => onChangeHandler(event)}
+                    />
                 </Form.Group>
                 <Form.Group>
-                    <Form.Control type="password" name="password" placeholder="Password" id="userPassword" defaultValue={password} onChange = {(event) => onChangeHandler(event)} />
+                    <Form.Control type="password" name="password" placeholder="Password" id="userPassword" defaultValue={password} onChange={(event) => onChangeHandler(event)} />
                 </Form.Group>
                 <Form.Group>
                     <Form.Check type="checkbox" label="Remember me" />
@@ -93,11 +105,12 @@ function Login(props) {
                 <p style={{ padding: "30px 0 0 0" }}>Not a member? <Link to="/create-account">Create.</Link></p>
             </Form>
             <div>
-                { user ? <p>{user.displayName}</p> : <p>Please sign in.</p> }
-                { user ? <Button onClick={signOut}>Sign out</Button> 
+                {user ? <p>{user.displayName}</p> : <p>Please sign in.</p>}
+                {user ? <Button onClick={signOut}>Sign out</Button>
                     : <Button onClick={signInWithGoogle}>
                         <span className="mr-2">Sign in with Google</span>
-                        <i className="fab fa-google"></i></Button>
+                        <i className="fab fa-google"></i>
+                    </Button>
                 }
             </div>
         </div>
@@ -106,4 +119,4 @@ function Login(props) {
 export default withFirebaseAuth({
     providers,
     firebaseAppAuth,
-  })(Login);
+})(Login);
